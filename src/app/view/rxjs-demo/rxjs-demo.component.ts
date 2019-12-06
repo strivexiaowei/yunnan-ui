9f992adbc6a0ca5bb218cab19d0443db6806bb5d
huangxianwei <1064599729@qq.com>
2019-12-06 13:04:02 +0800

    rxjs学习

--- src/app/view/rxjs-demo/rxjs-demo.component.ts
+++ src/app/view/rxjs-demo/rxjs-demo.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of, BehaviorSubject } from 'rxjs';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/reduce';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/filter';
+import { first, map, take, tap } from 'rxjs/operators';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-rxjs-demo',
   templateUrl: './rxjs-demo.component.html',
   styleUrls: ['./rxjs-demo.component.scss']
 })
 export class RxjsDemoComponent implements OnInit {
-
+  paracont = '倒计时10秒';
+  noClick = false;
+  keyValue: string;
+  inputValue: string;
+  mySubject: Subject<Event> = new Subject();
+  subject = new BehaviorSubject(0);
   persons = [
     { name: 'Dave', age: 34, salary: 2000 },
     { name: 'Nick', age: 37, salary: 32000 },
@@ -19,9 +27,25 @@ export class RxjsDemoComponent implements OnInit {
     { name: 'Kevin', age: 47, salary: 24000 },
   ];
   subjectStr$ = new Subject();
-  constructor() { }
+  constructor() {
+    // subscriber 1
+    this.mySubject.filter(({ type }) => type === 'keyup')
+      .map(e => (<KeyboardEvent>e).key)
+      .subscribe(value => this.keyValue = value);
+
+    // subscriber 2
+    this.mySubject.filter(({ type }) => type === 'input')
+      .map(e => (<HTMLInputElement>e.target).value)
+      .subscribe(value => this.inputValue = value);
+      this.subject.subscribe({
+        next: v => console.log(`Observer1: ${v}`)
+    })
+  }
 
   ngOnInit() {
+    this.subjectStr$.subscribe(res => {
+      console.log(res);
+    });
   }
 
   getFrom() {
@@ -66,20 +90,20 @@ export class RxjsDemoComponent implements OnInit {
   getCatch() {
     console.log('错误处理');
     this.getFromGoogle()
-    .catch(err => {
+      .catch(err => {
         console.error(`Error: ${err.status}: ${err.message}`);
         if (err.status === 404) {
-            return this.getFromBaidu();
+          return this.getFromBaidu();
         } else {
-            return this.getFromBing();
+          return this.getFromBing();
         }
-    })
-    .map(x => `The site is : ${x}`)
-    .subscribe(
+      })
+      .map(x => `The site is : ${x}`)
+      .subscribe(
         x => console.log('Subscriber got: ' + x),
         err => console.error(err),
         () => console.log('The stream is over.')
-    );
+      );
   }
 
   getFromGoogle(): Observable<any> {
@@ -100,10 +124,53 @@ export class RxjsDemoComponent implements OnInit {
     });
   }
 
+  getMapOf() {
+    map((x: number) => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`));
+  }
+
+  getFirstOf() {
+    first()(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`));
+  }
+
   getFromBaidu(): Observable<any> {
     return Observable.create(function subscribe(observer) {
       observer.next('https://www.baidu.com');
       observer.complete();
     });
   }
+
+  getSubject() {
+    this.subjectStr$.next('你是真的好么');
+  }
+
+  getPipe($event) {
+    // Observable.from($event).pipe(
+    //   take(1),
+    //   tap(v => console.log(v))
+    // )
+  }
+
+  getTake() {
+    // 每1秒发出值
+    const interval$ = interval(1000);
+    // 取前5个发出的值
+    const example = interval$.pipe(take(10));
+    // 输出: 0,1,2,3,4
+    const subscribe = example.subscribe(
+      val => {
+        this.noClick = true;
+        this.paracont = `倒计时${10 - val}秒`
+      },
+      error => { },
+      () => {
+        this.noClick = false;
+        this.paracont = '重新发送验证码'
+      }
+    );
+  }
+
+  getBehaviorSubject() {
+    this.subject.next(2);
+  }
+
 }
--- src/app/yunnan-ui/button/button.component.ts
+++ src/app/yunnan-ui/button/button.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   private size = 'default';
   private type = 'default';
+  private disabled = false;
   @Input()
   set ynSize(ynSize: string) {
     this.size = ynSize;
@@ -16,12 +17,20 @@ export class ButtonComponent implements OnInit {
   get ynSize(): string {
     return this.size;
   }
+
   @Input()
   set ynType(ynType: string) {
     this.type = ynType;
   }
   get ynType(): string {
     return this.type;
+  }
+  @Input()
+  set yndisabled(yndisabled: boolean) {
+    this.disabled = yndisabled;
+  }
+  get yndisabled(): boolean {
+    return this.disabled;
   }
   constructor() { }
 
