6cccdde1979e3ad0745af3f8d8e0c87c7a21c1b1
huangxianwei <1064599729@qq.com>
2019-12-13 10:19:57 +0800

    修改tabs

--- src/app/yunnan-ui/tabs/tabs.component.ts
+++ src/app/yunnan-ui/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, AfterViewInit, ChangeDetectorRef, HostListener } from '@angular/core';
 import { observable, from, Observable } from 'rxjs';
 import { reduce, map } from 'rxjs/operators';
 @Component({
@@ -16,6 +16,11 @@ export class TabsComponent implements OnInit, AfterViewInit {
   constructor(private el: ElementRef, private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    console.log('hehe');
+    this.ngAfterViewInit();
   }
 
   ngAfterViewInit(): void {
@@ -42,17 +47,18 @@ export class TabsComponent implements OnInit, AfterViewInit {
     console.log(arrowType);
     const { boxDomLeft } = this;
     if (arrowType === 'left') {
-      if (Math.abs(boxDomLeft) < 40) {
+      if (Math.abs(boxDomLeft) < 100) {
         this.boxDomLeft = 0;
       } else {
-        this.boxDomLeft = boxDomLeft + 40;
+        this.boxDomLeft = boxDomLeft + 100;
       }
     } else {
-      const numFlag = boxDomLeft + this.viewDomWidth > this.boxDomWidth + 40 + 64;
+      console.log(this.boxDomWidth - this.viewDomWidth - 64 - 100);
+      const numFlag = Math.abs(boxDomLeft) > this.boxDomWidth - this.viewDomWidth + 64 - 100;
       if (numFlag) {
-        this.boxDomLeft = this.boxDomWidth - this.viewDomWidth - 64;
+        this.boxDomLeft = -(this.boxDomWidth - this.viewDomWidth + 64);
       } else {
-        this.boxDomLeft = boxDomLeft - 40;
+        this.boxDomLeft = boxDomLeft - 100;
       }
       console.log(this.boxDomLeft);
     }
